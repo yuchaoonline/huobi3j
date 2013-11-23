a951c39937cd1059815adab64769a6c70732e52b
adeewu <sam65718@hotmail.com>
2013-11-24 00:08:25 +0800

    添加手机web api项目

---
+++ ADeeWu.HuoBi3J.MobileService/Areas/HelpPage/SampleGeneration/SampleDirection.cs
@@ -0,0 +1,11 @@
+namespace ADeeWu.HuoBi3J.MobileService.Areas.HelpPage
+{
+    /// <summary>
+    /// Indicates whether the sample is used for request or response
+    /// </summary>
+    public enum SampleDirection
+    {
+        Request = 0,
+        Response
+    }
+}
